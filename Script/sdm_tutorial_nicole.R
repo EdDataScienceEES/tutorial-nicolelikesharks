77b8b4568e980f0addc0ee3f4b8200741ff40c81
nicolelikesharks <71835073+nicolelikesharks@users.noreply.github.com>
2020-12-03 18:43:36 +0000

    Nicole commits script and additional files

---
+++ Script/sdm_tutorial_nicole.R
@@ -0,0 +1,262 @@
+# Introduction---- 
+# Species distribution modelling (SDM) Tutorial
+# Nicole Yap 
+# 29/11/2020
+
+
+# Libraries---- 
+
+library(tidyr)
+library(dplyr)
+library(leaflet)
+library(sdmpredictors)
+library(sp)
+library(sf)
+library(rgdal)
+library(raster)
+library(ggplot2)
+library(ggthemes)
+library(viridis)
+library(rasterVis)
+library(maps)
+library(rworldmap)
+library(maptools)
+
+# Loading and preparing data---- 
+
+whale_sharks <- read.csv("Data/whale_sharks_ningaloo.csv")
+
+# Inspect whale shark occurence data
+
+head(whale_sharks)
+
+# Checking to see how many rows and columns there are 
+
+dim(whale_sharks) 
+
+# Viewing column names
+
+colnames(whale_sharks) 
+
+
+# Only keeping columns we need
+
+whale_sharks_latlong <- subset(whale_sharks, select=c(oid,latitude,longitude,num_animals)) 
+whale_sharks_latlong2 <- subset(whale_sharks, select=c(longitude,latitude))
+
+
+# Loading sea surface temperature range and chlorophyll minimum rasters
+
+temp_raster <- raster("Data/surface_temp.tif")
+chl_raster <- raster("Data/chl_min.tif")
+
+
+
+# Preliminary visualization----
+
+# Plot basic whale shark occurence points 
+
+(prelim_plot <- ggplot(whale_sharks_latlong, aes(x = longitude, y = latitude, 
+                                                 colour = num_animals)) +
+    geom_point())
+
+
+# Obtaining map data 
+
+world <- getMap(resolution = "low")
+world_aus <- world[world@data$ADMIN=='Australia',] # Getting map for Australia
+
+
+# Plotting points on australia map
+
+(whale_sharks_map <- ggplot() +
+    borders("world", xlim = c(113, 154), ylim = c(-44, -10),
+            colour = "gray40", fill = "gray75", size = 0.3) +
+    geom_polygon(data = world_aus, 
+                 aes(x = long, y = lat, group = group),
+                 fill = NA, colour = "blue") + 
+    geom_point(data = whale_sharks_latlong,  # Add and plot species data
+               aes(x = longitude, y = latitude, 
+                   colour = num_animals)) +
+    scale_colour_viridis(option = "inferno") +
+    coord_quickmap() +  # Prevents stretching when resizing
+    theme_map() +  # Remove ugly grey background
+    xlab("Longitude") +
+    ylab("Latitude") + 
+    guides(colour=guide_legend(title="Number of whale sharks observed")))
+
+
+
+# Rebuild temperature layer to match chlorophyll layer
+
+new_chl <- raster(vals=values(chl_raster),ext=extent(temp_raster), nrows=dim(temp_raster)[1],ncols=dim(temp_raster)[2])
+
+
+# Determine the geographic extent of our data 
+
+max.lat = ceiling(max(whale_sharks_latlong$latitude))
+min.lat = floor(min(whale_sharks_latlong$latitude))
+max.lon = ceiling(max(whale_sharks_latlong$longitude))
+min.lon = floor(min(whale_sharks_latlong$longitude))
+geographic_extent <- extent(x = c(min.lon, max.lon, min.lat, max.lat))
+
+
+# Creating new raster predictor stack---- 
+
+predictors <- stack(temp_raster, new_chl)
+pred_crop <- crop(predictors, geographic_extent) # Cropping predictor stack using geographic extend of data
+
+
+pdf('Output/cropped_predictor_stack.pdf') # Saving cropped predictor stack
+plot(pred_crop) # Viewing cropped predictor stack
+dev.off()
+
+pdf('Output/chl_layer.pdf') # Saving chlorophyll layer 
+plot(pred_crop, 2) # Viewing second layer of stack (chlorophyll minimum layer)
+dev.off()
+
+pdf('Output/temp_layer.pdf') # Saving temperature layer 
+plot(pred_crop, 1) # Viewing first layer of stack (surface temperature range layer)
+dev.off()
+
+
+# Plot occurence points on chlorophyll layer 
+
+pdf('Output/chl_points_map.pdf') # Saving whale shark occurence points on chlorophyll map
+chl_ws_map <- points(whale_sharks_latlong2, col='blue')
+dev.off()
+
+pdf('Output/temp_points.pdf') # Saving whale shark occurence points on sst map
+temp_ws_map <- points(whale_sharks_latlong2, col='blue')
+dev.off()
+
+
+# Extract values of the predictors at the locations of the points--- 
+
+# Using extract() for whale shark occurrence points
+extract_pres_ws <- raster::extract(pred_crop, whale_sharks_latlong2)
+
+# Setting random seed to always create the same random set of points
+set.seed(0)
+
+
+# Extract values of the predictors for the 500 random background points 
+backgr <- randomPoints(pred_crop, 500)
+extract_abs_pred <- raster::extract(pred_crop, backgr)
+
+
+# Combining these extracted values into a single dataframe 
+
+pb <- c(rep(1, nrow(extract_pres_ws)), rep(0, nrow(extract_abs_pred)))
+sdmdata <- data.frame(cbind(pb, rbind(extract_pres_ws, extract_abs_pred)))
+
+
+# 
+sdmdata(,'biome') = as.factor(sdmdata(,'biome'))
+head(sdmdata)
+
+
+
+
+
+# Inspect available datasets and layers
+
+# Filter out terrestrial datasets 
+
+#datasets <- list_datasets(terrestrial = FALSE, marine = TRUE)
+#layers <- list_layers(datasets)
+
+
+
+
+
+# Loading equal sea surface temperature range and chlorophyll minimum layers 
+
+equal_layers <- load_layers(c("BO_sstrange","BO_chlomin"), equalarea=TRUE)
+
+# Cropping extent to that of Baltic Sea
+                            
+temp_chl_maps <- raster::crop(equal_layers, extent(106e5,154e5, -52e5, -13e5))
+
+# Plotting
+
+plot(temp_chl_maps)
+
+
+
+
+
+
+# Creating model----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#bioclim_data <- getData(name="worldclim", 
+                        var= "bio", 
+                        res= 2.5, 
+                        path="Data/")
+
+
+
+
+# Cropping bioclim data to geographic extent of whale sharks 
+
+#bioclim_data <- crop(x=bioclim_data, y= geographic_extent)
+
+#  Reverse order of columns
+#whale_sharks_latlong2 <- whale_sharks_latlong2[, c("longitude", "latitude")]
+
+
+# Building species distribution model 
+
+#bioclim_model <- bioclim(x = bioclim_data, p = whale_sharks_latlong2)
+
+
+# Predict presence from model
+#predict_presence <- dismo::predict(object = bioclim_model, 
+                                   x = bioclim_data, 
+                                   ext = geographic_extent)
+
+
+# map 
+
+# Plotting points on map 
+
+#plot(world_aus, 
+     xlim = c(min.lon, max.lon),
+     ylim = c(min.lat, max.lat),
+     axes = TRUE, 
+     col = "grey95")
+
+# Add model probabilities
+#plot(predict_presence, add = TRUE)
+
+#plot(world_aus, add = TRUE, border = "grey5")
+
+
+#points(whale_sharks_latlong2$longitude, whale_sharks_latlong2$latitude, col = "olivedrab", pch = 20, cex = 0.75)
+box()
+
+
